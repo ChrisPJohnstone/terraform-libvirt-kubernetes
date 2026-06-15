1d9904c5abbf3308f1a035659947e8e046a8b218
Christopher Johnstone <chrisj97@proton.me>
2026-06-15 14:43:50 +0100

    Disabled swap on node

--- terraform/modules/worker_node/main.tf
+++ terraform/modules/worker_node/main.tf
@@ -31,6 +31,11 @@ resource "libvirt_cloudinit_disk" "guest_seed" {
       - curl
       - gpg
     runcmd:
+      # Disable Swap
+      - swapoff -a
+      - sed -i '/ swap / s/^/#/' /etc/fstab
+
+      # Install Kubelet
       - curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
       - echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
       - sudo apt-get update
